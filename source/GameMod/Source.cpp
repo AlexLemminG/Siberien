ecc74740e77ce203c6415516c0c29981fe827ac3
Alexander <psp_4ever@mail.ru>
2021-08-26 00:04:50 +0300

    assimp importers goes plup

---
+++ source/GameMod/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Engine.h"
+#include "Serialization.h"
+#include "Component.h"
+#include "System.h"
+
+class GameLib : public GameLibrary {
+	virtual bool Init(Engine* engine) override {
+		OPTICK_EVENT();
+		if (!GameLibrary::Init(engine)) {
+			return false;
+		}
+		std::cout << "Hello there" << std::endl;
+		return true;
+	}
+	INNER_LIBRARY();
+};
+
+class Sys : public GameSystem<Sys> {
+	bool Init()override {
+		std::cout << "General kenobi!" << std::endl;
+		return true;
+	}
+};
+REGISTER_GAME_SYSTEM(Sys);
+DEFINE_LIBRARY(GameLib);
